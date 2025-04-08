4521aebde698d7e3cd52016474f222c3cfa83876
Dimasik <ryzovdima@gmail.com>
2025-04-08 18:26:34 +0300

    make_profile

---
+++ app/src/main/java/com/example/sharedbooking/fragments/EditProfile.kt
@@ -0,0 +1,32 @@
+package com.example.sharedbooking.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.sharedbooking.R
+import com.example.sharedbooking.databinding.FragmentEditProfileBinding
+
+class EditProfile : Fragment() {
+    private lateinit var binding: FragmentEditProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
